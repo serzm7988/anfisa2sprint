50929e13e964e82c78346b556065a2179e1e45e5
Serzm7988 <119091549+serzm7988@users.noreply.github.com>
2024-10-30 11:47:14 +1100

    Start project

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, IceCream, Topping, Wrapper
+
+admin.site.register(Topping)
+admin.site.register(Wrapper)
+admin.site.register(IceCream)
+admin.site.register(Category)
